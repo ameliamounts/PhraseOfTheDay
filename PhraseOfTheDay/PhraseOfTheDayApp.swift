fb3edbd765d07939027712d1ca585bcdd3a6dc0e
Amelia Mounts <ameliamounts@gmail.com>
2024-09-20 08:25:20 -0400

    Initial Commit

---
+++ PhraseOfTheDay/PhraseOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhraseOfTheDayApp.swift
+//  PhraseOfTheDay
+//
+//  Created by Amelia Mounts on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PhraseOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
